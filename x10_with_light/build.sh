22b5a2027fb124c9b1ebd19b9e18e116e75bdfe8
Jason Cox <j_cox@globelock.dyndns.info>
2013-06-21 16:54:06 +1000

    loop until its uploaded

--- x10_with_light/build.sh
+++ x10_with_light/build.sh
@@ -3,6 +3,13 @@ cd `dirname $0`
 rm -fr build-cli*
 make clean
 make
-make  upload
+x=1
+while [ $x -ne 0 ]
+do
+        make upload
+        x=`echo $?`
+        sleep 2
+done
+
 rm -fr build-cli*
 
