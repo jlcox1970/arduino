546ff6bd0d0a5be6622dfda6ab8403a7256e6be8
Jason Cox <j_cox@globelock.dyndns.info>
2013-08-11 15:40:22 +1000

    new x10 code from net for v101

--- library-x10/x10.h
+++ library-x10/x10.h
@@ -1,8 +1,19 @@
 /*
+  x10.cpp - X10 transmission library for Arduino version 0.4
   
-  Original library		(0.1) by Tom Igoe.
-  Timing bug fixes		(0.2) "   "   "
-  Temperature 			(0.3) by Jason Cox
+ Copyright (c) 2007 by Tom Igoe (tom.igoe@gmail.com)
+
+This file is free software; you can redistribute it and/or modify
+it under the terms of either the GNU General Public License version 2
+or the GNU Lesser General Public License version 2.1, both as
+published by the Free Software Foundation.
+
+ 
+  Original library								(0.1) 
+  Timing bug fixes								(0.2)
+  #include bug fixes for 0012					(0.3)
+  Refactored version following Wire lib API		(0.4)
+
 	Sends X10 commands.
 
 */
@@ -11,31 +22,50 @@
 #ifndef x10_h
 #define x10_h
 
-// include types & constants of Wiring core API
-#include <stdlib.h>
-#include "Arduino.h"
-#include "pins_arduino.h"
+// include types & constants of core API (for Arduino after 0022)
+#include <Arduino.h>
+#include "x10constants.h"
 
 // library interface description
-class x10 {
+class x10Class : public Stream
+{
   public:
     // constructors:
-    x10(int zeroCrossingPin, int dataPin);
+    x10Class();
+ 	void begin(int _rxPin=3,int _txPin=4, int _zcPin=2);
 
-    // write command method:
-	void write(byte houseCode, byte numberCode, int numRepeats);
-    // returns the version number:
-    int version(void);
-    void x10temp(byte tmep_houseCode, byte temp_Unit , int count, int RPT_SEND);
+
+    void beginTransmission(uint8_t data);
+    void beginTransmission(int data);
+    void endTransmission(void);
+   // uint8_t requestFrom(uint8_t, uint8_t);	
+   // uint8_t requestFrom(int, int);
+    virtual size_t write(uint8_t data);
+    virtual size_t write(const char *data);
+    virtual size_t write(const uint8_t *, size_t);
+    
+    // the following are not implemented yet:
+    virtual int available(void);
+    virtual int read(void);
+    virtual int peek(void);
+	virtual void flush(void);
+   // void onReceive( void (*)(int) );
+   // void onRequest( void (*)(void) );
 
   private:
-  	int zeroCrossingPin;	// AC zero crossing pin
-  	int dataPin;			// data out pin
+  	static uint8_t zeroCrossingPin;	// AC zero crossing pin
+  	static uint8_t txPin;			// data out pin
+  	static uint8_t rxPin;			// data in pin
+  	static uint8_t houseCode;		// house code
+  	static uint8_t transmitting;	// whether or not you're transmitting
+ 
   	// sends the individual bits of the commands:
-    void sendBits(byte cmd, byte numBits, byte isStartCode);
-    // checks for AC zero crossing
-    void waitForZeroCross(int pin, int howManyTimes);
+    void sendBits(byte cmd, byte numBits, byte isStartCode);	// does bit shifting of a command
+    void sendCommand(byte houseCode, byte numberCode);			// sends a command
+    void waitForZeroCross(int pin, int howManyTimes);		    // checks for AC zero crossing
 };
+
+extern x10Class x10;
 
 #endif
 
